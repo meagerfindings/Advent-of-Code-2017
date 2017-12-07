8d302c3bcb93f8bb976d4565025540e236ae8dae
Mat Greten <matgreten@gmail.com>
2017-12-06 19:44:20 -0700

    finished day 3, puzzle 2

--- day_3_sprial_memory/README.md
+++ day_3_sprial_memory/README.md
@@ -120,4 +120,105 @@ Once a square is written, its value does not change. Therefore, the first few sq
 ### Task
 What is the **first value written** that is **larger** than your puzzle input?
 
+Next value is: `295229`
+
 _Your puzzle input is still_ `289326`.
+
+#### My Solution
+
+```
+require 'pp'
+
+input = ARGV[0].to_i
+
+@target_number = input
+@target_reached = false
+@one_beyond = 0
+
+@x = 0
+@y = 0
+@n = 1
+@rotation_number = 1
+
+@spiral_hash = { @x.to_s + @y.to_s => @n }
+
+def right
+  @n = check_blocks_around((@x += 1).to_s + @y.to_s)
+  @spiral_hash[@x.to_s + @y.to_s] = @n
+  destination_check
+end
+
+def up
+  @n = check_blocks_around(@x.to_s + (@y += 1).to_s)
+  @spiral_hash[@x.to_s + @y.to_s] = @n
+  destination_check
+end
+
+def left
+  @n = check_blocks_around((@x -= 1).to_s + @y.to_s)
+  @spiral_hash[@x.to_s + @y.to_s] = @n
+  destination_check
+end
+
+def down
+  @n = check_blocks_around(@x.to_s + (@y -= 1).to_s)
+  @spiral_hash[@x.to_s + @y.to_s] = @n
+  destination_check
+end
+
+def rotate
+  right_up_num = 2 * @rotation_number - 1
+  right_up_num.times { right }
+  right_up_num.times { up }
+
+  left_down_num = 2 * @rotation_number
+  left_down_num.times { left }
+  left_down_num.times { down }
+
+  @rotation_number += 1
+end
+
+def create_spiral(input)
+  rotate until @n >= input
+end
+
+def check_blocks_around(location)
+  val = 0
+  unless @spiral_hash[(@x - 1).to_s + (@y - 1).to_s].nil?
+    val += @spiral_hash[(@x - 1).to_s + (@y - 1).to_s]
+  end
+  unless @spiral_hash[(@x - 1).to_s + (@y + 0).to_s].nil?
+    val += @spiral_hash[(@x - 1).to_s + (@y + 0).to_s]
+  end
+  unless @spiral_hash[(@x - 1).to_s + (@y + 1).to_s].nil?
+    val += @spiral_hash[(@x - 1).to_s + (@y + 1).to_s]
+  end
+  unless @spiral_hash[(@x + 0).to_s + (@y - 1).to_s].nil?
+    val += @spiral_hash[(@x + 0).to_s + (@y - 1).to_s]
+  end
+  unless @spiral_hash[(@x + 0).to_s + (@y + 1).to_s].nil?
+    val += @spiral_hash[(@x + 0).to_s + (@y + 1).to_s]
+  end
+  unless @spiral_hash[(@x + 1).to_s + (@y - 1).to_s].nil?
+    val += @spiral_hash[(@x + 1).to_s + (@y - 1).to_s]
+  end
+  unless @spiral_hash[(@x + 1).to_s + (@y + 0).to_s].nil?
+    val += @spiral_hash[(@x + 1).to_s + (@y + 0).to_s]
+  end
+  unless @spiral_hash[(@x + 1).to_s + (@y + 1).to_s].nil?
+    val += @spiral_hash[(@x + 1).to_s + (@y + 1).to_s]
+  end
+  val
+end
+
+def destination_check
+  if @target_reached == false && @target_number < @n
+    @one_beyond = @n
+    @target_reached = true
+  end
+end
+
+create_spiral(input)
+pp @spiral_hash
+pp @one_beyond
+```
--- day_3_sprial_memory/day_3_puzzle_2.rb
+++ day_3_sprial_memory/day_3_puzzle_2.rb
@@ -2,41 +2,50 @@ require 'pp'
 
 input = ARGV[0].to_i
 
+@target_number = input
+@target_reached = false
+@one_beyond = 0
+
 @x = 0
 @y = 0
 @n = 1
 @rotation_number = 1
 
-@spiral_hash = { @n => [@x, @y] }
+@spiral_hash = { @x.to_s + @y.to_s => @n }
 
 def right
-  @spiral_hash[@n += 1] = [@x += 1, @y]
+  @n = check_blocks_around((@x += 1).to_s + @y.to_s)
+  @spiral_hash[@x.to_s + @y.to_s] = @n
+  destination_check
 end
 
 def up
-  @spiral_hash[@n += 1] = [@x, @y += 1]
+  @n = check_blocks_around(@x.to_s + (@y += 1).to_s)
+  @spiral_hash[@x.to_s + @y.to_s] = @n
+  destination_check
 end
 
 def left
-  @spiral_hash[@n += 1] = [@x -= 1, @y]
+  @n = check_blocks_around((@x -= 1).to_s + @y.to_s)
+  @spiral_hash[@x.to_s + @y.to_s] = @n
+  destination_check
 end
 
 def down
-  @spiral_hash[@n += 1] = [@x, @y -= 1]
+  @n = check_blocks_around(@x.to_s + (@y -= 1).to_s)
+  @spiral_hash[@x.to_s + @y.to_s] = @n
+  destination_check
 end
 
 def rotate
-  if @rotation_number == 1
-    right
-    up
-  else
-    right_up_num = 2 * @rotation_number - 1
-    right_up_num.times { right }
-    right_up_num.times { up }
-  end
+  right_up_num = 2 * @rotation_number - 1
+  right_up_num.times { right }
+  right_up_num.times { up }
+
   left_down_num = 2 * @rotation_number
   left_down_num.times { left }
   left_down_num.times { down }
+
   @rotation_number += 1
 end
 
@@ -44,14 +53,42 @@ def create_spiral(input)
   rotate until @n >= input
 end
 
-def calculate_steps(input)
-  steps = 0
-  @spiral_hash[input].each do |x|
-    steps += x.abs
+def check_blocks_around(location)
+  val = 0
+  unless @spiral_hash[(@x - 1).to_s + (@y - 1).to_s].nil?
+    val += @spiral_hash[(@x - 1).to_s + (@y - 1).to_s]
   end
-  steps
+  unless @spiral_hash[(@x - 1).to_s + (@y + 0).to_s].nil?
+    val += @spiral_hash[(@x - 1).to_s + (@y + 0).to_s]
+  end
+  unless @spiral_hash[(@x - 1).to_s + (@y + 1).to_s].nil?
+    val += @spiral_hash[(@x - 1).to_s + (@y + 1).to_s]
+  end
+  unless @spiral_hash[(@x + 0).to_s + (@y - 1).to_s].nil?
+    val += @spiral_hash[(@x + 0).to_s + (@y - 1).to_s]
+  end
+  unless @spiral_hash[(@x + 0).to_s + (@y + 1).to_s].nil?
+    val += @spiral_hash[(@x + 0).to_s + (@y + 1).to_s]
+  end
+  unless @spiral_hash[(@x + 1).to_s + (@y - 1).to_s].nil?
+    val += @spiral_hash[(@x + 1).to_s + (@y - 1).to_s]
+  end
+  unless @spiral_hash[(@x + 1).to_s + (@y + 0).to_s].nil?
+    val += @spiral_hash[(@x + 1).to_s + (@y + 0).to_s]
+  end
+  unless @spiral_hash[(@x + 1).to_s + (@y + 1).to_s].nil?
+    val += @spiral_hash[(@x + 1).to_s + (@y + 1).to_s]
+  end
+  val
+end
+
+def destination_check
+  if @target_reached == false && @target_number < @n
+    @one_beyond = @n
+    @target_reached = true
+  end
 end
 
 create_spiral(input)
-# pp @spiral_hash
-pp "Steps taken: #{calculate_steps(input)}"
+pp @spiral_hash
+pp @one_beyond
